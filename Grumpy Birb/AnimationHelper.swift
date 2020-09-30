f8282c127fad9273011def6a3288b95833188c72
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-09-30 12:05:56 -0600

    adds animation to birds in flight

---
+++ Grumpy Birb/AnimationHelper.swift
@@ -0,0 +1,23 @@
+//
+//  AnimationHelper.swift
+//  Grumpy Birb
+//
+//  Created by David Schlundt-Bodien on 9/30/20.
+//  Copyright © 2020 David Schlundt-Bodien. All rights reserved.
+//
+
+import SpriteKit
+
+class AnimationHelper {
+    static func loadTextures(from atlas:SKTextureAtlas, withName name: String) -> [SKTexture] {
+        var textures = [SKTexture]()
+        
+        for index in 0..<atlas.textureNames.count {
+            let textureName = name + String(index+1)
+            textures.append(atlas.textureNamed(textureName))
+        }
+        
+        
+        return textures
+    }
+}
